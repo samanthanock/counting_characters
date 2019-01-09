a69b645f63d6045fb141430908197f478e032066
samanthanock <sammarienock@gmail.com>
2019-01-09 23:00:40 +0000

    this is an update

--- counting-char.js
+++ counting-char.js
@@ -3,12 +3,14 @@ function countLetters(str) {
   var newObject = {};
   for (var i = 0; i < newArray.length; i++) {
     var letter = newArray[i];
+
     if (newObject[letter]) {
       newObject[letter]++;
     } else {
       newObject[letter] = 1;
     }
+    delete newObject[''];
   }
   return newObject;
 }
-console.log(countLetters('Samantha Marie Nock'));
+console.log(countLetters('Samantha Mrie Nock'));
